4714beb8392a19a30f39611d380fbb5725e3926a
KorolenkoDaria <admem75213@gmail.com>
2023-08-04 12:04:56 +0300

    slider connection

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,5 @@
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 
 const swiper = new Swiper('.yacht-slider', {
@@ -28,5 +28,25 @@ const swiper = new Swiper('.yacht-slider', {
     }
 });
 
-// let next = document.querySelector(".swiper-button-next");
-// next.addEventListener("click", swiper.slideNext());
+const swiper2 = new Swiper('.rev-slider', {
+    modules: [Pagination],
+    loopedSlides: 1,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 32
+        },
+    }
+});
